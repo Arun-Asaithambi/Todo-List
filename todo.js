0274d846a1adf01a1926dc5f90c203fbf79e8c16
Arun-Asaithambi <167624082+Arun-Asaithambi@users.noreply.github.com>
2024-09-12 15:37:04 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,14 @@
+let todoList = document.getElementById('todolist')   
+let input = document.getElementsById("input")
+let add = document.getElementById('add')
+
+let todos = [];
+
+butoon.addEventListner('click', () => {
+    todos.push(input)
+    addtodo(input.value)
+})
+
+function addtodo(todo){
+
+}
